92548e211095ebdc858baf67004490acff46fd5f
ctrlprogrammer <sebastianto1999@gmail.com>
2024-09-11 03:55:00 -0500

    Dotenv

--- main.go
+++ main.go
@@ -4,16 +4,24 @@ import (
 	core "EVM/EventListener/core"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/joho/godotenv"
 )
 
 func listenEvent(log types.Log, event *core.ConnectorEventListener, network *core.Network) {
-	fmt.Println("New log call " + event.Name)
+	fmt.Println("New log call " + event.Name + " from " + network.Name)
 }
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
 	contracts := []common.Address{common.HexToAddress("0xB8981C1E85f5Acfbf1760Cb4DB3933526d8a269e")}
 
 	listeners := make(map[string]core.ConnectorEventListener)
@@ -23,7 +31,7 @@ func main() {
 		Topic: common.HexToHash("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"),
 	}
 
-	arbNetwork, err := core.NewNetwork("Arbitrum Sepolia", "wss://arb-sepolia.g.alchemy.com/v2/6QUL7GY-FZKzuH6OouFvQHbhuqYugJQ-", listeners, contracts, listenEvent)
+	arbNetwork, err := core.NewNetwork("Arbitrum Sepolia", os.Getenv("SEPOLIA_RPC"), listeners, contracts, listenEvent)
 
 	if err != nil {
 		log.Fatal(err)
